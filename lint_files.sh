f61b8fe2fa2c940dff0bec587922afae655f2c93
philipp08888 <admin@fledi.dev>
2025-05-16 22:50:54 +0200

    feat: improve lint files script

--- lint_files.sh
+++ lint_files.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-echo "All arguments:"
-echo "$@"
+LUACHECK_ARGS="--config /luacheck-fivem/.luacheckrc $1"
+LUACHECK_PATHS="$2"
+LUACHECK_CAPTURE="$3"
+LUACHECK_EXTRA_LIBS="$4"
+IGNORED_SCRIPTS="$5"
+
+# build .luacheckrc file
+RESOURCES_PATH=$LUACHECK_PATHS IGNORED_SCRIPTS=$IGNORED_SCRIPTS yarn build
+
+echo "outfile => $LUACHECK_CAPTURE_OUTFILE"
+
+if [[ ! -z "$LUACHECK_CAPTURE_OUTFILE" ]]; then
+  echo "exec => luacheck $LUACHECK_ARGS $LUACHECK_PATH 2>>$LUACHECK_CAPTURE_OUTFILE"
+  luacheck $LUACHECK_ARGS $LUACHECK_PATH >$LUACHECK_CAPTURE_OUTFILE 2>&1 || true
+  
+  echo "exec => luacheck $LUACHECK_ARGS --formatter default $LUACHECK_PATH"
+  luacheck $LUACHECK_ARGS --formatter default $LUACHECK_PATH || EXIT_CODE=$?
+else
+  echo "exec => luacheck $LUACHECK_ARGS $LUACHECK_PATH"
+  luacheck $LUACHECK_ARGS $LUACHECK_PATH || EXIT_CODE=$?
+fi
+
+echo "exit => $EXIT_CODE"
+if [ $EXIT_CODE -ge 2 ]; then
+ exit $EXIT_CODE
+fi
